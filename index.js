c35fa5e36c53f199f32eaa43f6aa4bce4353698e
SabrinaSzemberg <sabrina.schenberg@gmail.com>
2022-07-04 13:09:41 -0300

    Add cool face API

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3001
 
-app.get('/', (req, res) => {
-  res.send('Tá rodando no heroku!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Tá rodando no heroku!')
+// })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
